4bcf094a55d09d7972c3cec0501e53c63df72939
strangerpings <13926900+vkar-hub@users.noreply.github.com>
2025-11-22 07:52:41 +0530

    Added ivy, counsel and swiper.

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; init.el --- Minimal Workshop Emacs Config
+;;; init.el --- Minimal Workshop Emacs Config (Org + Org-roam + Ivy)
 
 ;; -----------------------------------------
 ;; Package manager setup
@@ -15,7 +15,6 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Install use-package if missing
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
@@ -25,21 +24,46 @@
 ;; -----------------------------------------
 ;; Basic UI cleanup
 ;; -----------------------------------------
-(setq inhibit-startup-message t)   ; no welcome screen
+(setq inhibit-startup-message t)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (tooltip-mode -1)
 
-(global-visual-line-mode 1)        ; wrap lines naturally
-(setq visible-bell t)              ; no beep
+(global-visual-line-mode 1)
+(setq visible-bell t)
 
-;; A readable default font (safe)
 (set-face-attribute 'default nil :height 140)
 
 ;; Split windows vertically by default
 (setq split-height-threshold nil)
 (setq split-width-threshold 0)
+
+;; -----------------------------------------
+;; IVY + COUNSEL + SWIPER
+;; -----------------------------------------
+
+(use-package ivy
+  :diminish
+  :config
+  (ivy-mode 1)
+  :custom
+  (ivy-use-virtual-buffers t)
+  (ivy-count-format "(%d/%d) ")
+  (ivy-wrap t))
+
+(use-package counsel
+  :after ivy
+  :bind (("M-x"     . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("C-c f"   . counsel-recentf)
+         ("C-c g"   . counsel-git)
+         ("C-c k"   . counsel-rg)))
+
+(use-package swiper
+  :after ivy
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper)))
 
 ;; -----------------------------------------
 ;; Org Mode
@@ -50,7 +74,7 @@
   (org-hide-emphasis-markers t)
   (org-startup-with-inline-images t)
   (org-image-actual-width '(300))
-  (org-directory "~/org")                    ; default notes location
+  (org-directory "~/org")
   (org-agenda-files (directory-files-recursively "~/org" "\\.org$"))
   (org-todo-keywords '((sequence "TODO(t)" "WAIT(w)" "|" "DONE(d)" "CANCELLED(c)")))
   :bind (("C-c a" . org-agenda)
@@ -58,7 +82,6 @@
 
 (setq org-default-notes-file "~/org/inbox.org")
 
-;; Simple capture template
 (setq org-capture-templates
       '(("t" "Todo" entry (file "~/org/inbox.org")
          "* TODO %?\n  %U")))
@@ -68,7 +91,7 @@
 ;; -----------------------------------------
 (use-package org-roam
   :init
-  (setq org-roam-v2-ack t) ; prevent v2 upgrade prompt
+  (setq org-roam-v2-ack t)
   :custom
   (org-roam-directory (file-truename "~/org/roam"))
   :config
@@ -78,11 +101,10 @@
          ("C-c n i" . org-roam-node-insert)))
 
 ;; -----------------------------------------
-;; UTF-8 everywhere (avoids encoding bugs)
+;; UTF-8 everywhere
 ;; -----------------------------------------
 (prefer-coding-system 'utf-8)
 (set-language-environment "UTF-8")
 
 (provide 'init)
 ;;; init.el ends here
-
